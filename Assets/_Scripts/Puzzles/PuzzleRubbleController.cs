39458349ca8a1ef18698fb9edc9516036861e6ed
Oriol Miro-Barcelo <oriol.miro-barcelo@uni-ulm.de>
2018-07-23 10:57:41 +0200

    Added PuzzleRubble1

---
+++ Assets/_Scripts/Puzzles/PuzzleRubbleController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Shoguneko
+{
+    public class PuzzleRubbleController : MonoBehaviour
+    {
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                Grid.helper.ChangeScene("RubbleRoom1", "init");
+            }
+            else if (Input.GetKeyDown(KeyCode.Space))
+            {
+                //PlayerPrefs.DeleteKey("kitchen_mid");
+                PlayerPrefs.SetString("rubble", "true");
+                Grid.helper.ChangeScene("RubbleRoom2", "init");
+            }
+        }
+    }
+}
