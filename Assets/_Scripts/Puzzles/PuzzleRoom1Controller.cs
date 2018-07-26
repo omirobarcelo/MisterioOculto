4a180844e611ea6346bf369e218ff91ef2804d32
Oriol Miro-Barcelo <oriol.miro-barcelo@uni-ulm.de>
2018-07-26 15:58:36 +0200

    ,

---
+++ Assets/_Scripts/Puzzles/PuzzleRoom1Controller.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Shoguneko
+{
+    public class PuzzleRoom1Controller : MonoBehaviour
+    {
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                Grid.helper.ChangeScene("Room1_1", "init");
+            }
+            else if (Input.GetKeyDown(KeyCode.Space))
+            {
+                //PlayerPrefs.DeleteKey("kitchen_mid");
+                PlayerPrefs.SetString("room1", "true");
+                Grid.helper.ChangeScene("Room1_2", "init");
+            }
+        }
+    }
+}
